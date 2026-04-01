ac98e8a7040a24bbb6bc044b531a91cc00fc71ae
MLCARS <rafaelafilipa@yahoo.fr>
2026-04-01 19:59:26 +0200

    fix supabase cookies

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { cookies } from 'next/headers'
 import { createServerClient } from '@supabase/ssr'
 
-export function getSupabaseServerClient() {
-  const cookieStore = cookies()
+export async function getSupabaseServerClient() {
+  const cookieStore = await cookies()
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL
   const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
@@ -14,7 +14,7 @@ export function getSupabaseServerClient() {
         return cookieStore.get(name)?.value
       },
       set() {},
-      remove() {}
-    }
+      remove() {},
+    },
   })
 }
